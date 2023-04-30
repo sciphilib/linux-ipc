65ac920af9ba4b2de580b8ee003a95c11808ee7f
Vladimir <sciphilib@gmail.com>
2023-04-30 18:39:58 +0700

    fix d1-10, done d2-1

---
+++ second/1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv) {
+  int counter = 0;
+  int wr_stream;
+  
+  if ((wr_stream = open("file.txt", O_CREAT | O_WRONLY, S_IRWXU)) == -1) {
+    perror("open() error ");
+    exit(EXIT_FAILURE);
+  }
+  
+  if (dup2(wr_stream, 1) == -1) {
+    perror("dup2() error ");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    printf("%d ", counter++);
+    sleep(1);
+  }
+  
+  close(wr_stream);
+  return 0;
+}
